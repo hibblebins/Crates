9aa265acd43c9f05b7c8532bb1d06007c1cb6a4e
WanderingMouse <2009monkeyman@gmail.com>
2025-03-09 13:07:12 -0700

    Added the mod, gonna start adding snortable diamond dust

--- src/main/java/opileos/crates/OpileosCratesMod.java
+++ src/main/java/opileos/crates/OpileosCratesMod.java
@@ -2,6 +2,9 @@ package opileos.crates;
 
 import net.fabricmc.api.ModInitializer;
 
+import opileos.crates.block.ModBlocks;
+import opileos.crates.item.ModItemGroups;
+import opileos.crates.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,10 +18,10 @@ public class OpileosCratesMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
 	}
 }
---
+++ src/main/java/opileos/crates/item/ModItemGroups.java
@@ -0,0 +1,24 @@
+package opileos.crates.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import opileos.crates.OpileosCratesMod;
+import opileos.crates.block.ModBlocks;
+
+public class ModItemGroups {
+    public static final ItemGroup CRATES_GROUP = Registry.register(Registries.ITEM_GROUP, new Identifier(OpileosCratesMod.MOD_ID, "crates"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.crates"))
+                    .icon(() -> new ItemStack(ModItems.DIAMOND_DUST)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.DIAMOND_DUST);
+                        entries.add(ModBlocks.CRATE);
+                    }).build());
+
+    public static void registerItemGroups() {
+        OpileosCratesMod.LOGGER.info("Registering Item Groups for " + OpileosCratesMod.MOD_ID);
+    }
+}
